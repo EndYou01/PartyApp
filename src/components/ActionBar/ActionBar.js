ea529d11f62cc97885a80745f7a446f7864c9c11
Eduardo Garcia Alfonso <eduardojga.oficina@gmail.com>
2023-01-03 13:09:57 -0500

    fixed imgsrc imports

--- src/components/ActionBar/ActionBar.js
+++ src/components/ActionBar/ActionBar.js
@@ -2,40 +2,33 @@ import React from 'react';
 import { Button } from '../Buttons/Button';
 import {ButtonWhitCounter} from '../Buttons/ButtonWhitCounter';
 
-// import heartIcon from '../../assets/actionBar/heart.png';
-// import heartRedIcon from '../../assets/actionBar/heartRed.png';
-// import addIcon from '../../assets/actionBar/add.png';
-// import addBlackIcon from '../../assets/actionBar/addBlack.png';
-// import ticketsIcon from '../../assets/actionBar/tickets.png';
-// import ticketsBlackIcon from '../../assets/actionBar/ticketsBlack.png';
-// import commentsIcon from '../../assets/actionBar/bubbleChat.png';
+
+
 
 export const ActionBar = () => {
 	
-
-
 	return (
 		<div>
 			<ul className='listActionBar'>
 
 					<li><ButtonWhitCounter 
-					src={heartIcon} 	 
-					srcAct={heartRedIcon} 
+					src={'assets/actionBar/heart.png'} 	 
+					srcAct={'assets/actionBar/heartRed.png'} 
 					alt={'heart Icon'}/>
 					</li>
 					
 					<li><Button 
-							src={addIcon}
-							srcActive={addBlackIcon}
+							src={'assets/actionBar/add.png'}
+							srcActive={'assets/actionBar/addBlack.png'}
 							alt='add Icon'/></li>
 					
 					<li><Button 
-					src={ticketsIcon} 
-					srcActive={ticketsBlackIcon}
+					src={'assets/actionBar/tickets.png'} 
+					srcActive={'assets/actionBar/ticketsBlack.png'}
 					alt='tickets Icon '/></li>	
 					
 					<li><Button 
-					src={commentsIcon} 
+					src={'assets/actionBar/bubbleChat.png'} 
 					alt='comments Icon'/></li>						
 
 			 </ul>
--- src/hooks/useActive.js
+++ src/hooks/useActive.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { useCounter } from './useCounter'
 
 export const useActive = () => {
